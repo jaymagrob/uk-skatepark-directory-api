96bbfed1a5078a592349ee9312b8f56a9de613aa
jaymagrob <johnmagnusrobertson@gmail.com>
2020-02-04 11:35:02 +0000

    seeds.js mispelling

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODBD_URI || 'mongodb://localhost/ski-resorts-api'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/ski-resorts-api'
 const secret = process.env.SECRET || 'THIS IS A SECRET'
 
 module.exports = { port, dbURI, secret }
